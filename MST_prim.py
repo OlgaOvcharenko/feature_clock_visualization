4b1c89ae75c9878e5ee6ac00e97fb5928d5e775b
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-10-11 11:30:09 +0200

    Add MST

---
+++ MST_prim.py
@@ -0,0 +1,162 @@
+import csv
+import random
+import sys
+from joblib import Parallel, delayed
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.spatial import distance_matrix
+
+# from mnist_umap_phate import hdbscan_low_dim
+
+
+class Graph:
+    """https://stackabuse.com/courses/graphs-in-python-theory-and-implementation/lessons/minimum-spanning-trees-prims-algorithm/"""
+    
+    def __init__(self, num_of_nodes):
+        self.m_num_of_nodes = num_of_nodes
+        # Initialize the adjacency matrix with zeros
+        self.m_graph = [[0 for _ in range(num_of_nodes)] 
+                    for _ in range(num_of_nodes)]
+        
+    def _euclidian_distance(self, a, b):
+        return np.sqrt(np.sum(np.square(a - b)))
+        
+    def add_all_nodes(self, data):
+        def inner_loop(a, b, i, j):
+            self._add_edge(i, j, self._euclidian_distance(a, b))
+
+        for i in range(len(data)-1):
+            # combined_res = Parallel(n_jobs=-1)\
+            #     (delayed(inner_loop)\
+            #     (data[i], data[j], i, j) for j in range(i+1, len(data)))
+            for j in range(i+1, len(data)):
+                inner_loop(data[i], data[j], i, j)
+
+            
+
+    def _add_edge(self, node1, node2, weight):
+        self.m_graph[node1][node2] = weight
+        self.m_graph[node2][node1] = weight
+
+
+    def prims_mst(self):
+        # Defining a really big number, that'll always be the highest weight in comparisons
+        postitive_inf = float('inf')
+
+        # This is a list showing which nodes are already selected 
+        # so we don't pick the same node twice and we can actually know when stop looking
+        selected_nodes = [False for node in range(self.m_num_of_nodes)]
+
+        # Matrix of the resulting MST
+        result = [[0 for column in range(self.m_num_of_nodes)] 
+                    for row in range(self.m_num_of_nodes)]
+        
+        indx = 0
+        # for i in range(self.m_num_of_nodes):
+        #     print(self.m_graph[i])
+        
+        # print(selected_nodes)
+
+        # While there are nodes that are not included in the MST, keep looking:
+        while(False in selected_nodes):
+            # We use the big number we created before as the possible minimum weight
+            minimum = postitive_inf
+
+            # The starting node
+            start = 0
+
+            # The ending node
+            end = 0
+
+            for i in range(self.m_num_of_nodes):
+                # If the node is part of the MST, look its relationships
+                if selected_nodes[i]:
+                    for j in range(self.m_num_of_nodes):
+                        # If the analyzed node have a path to the ending node AND its not included in the MST (to avoid cycles)
+                        if (not selected_nodes[j] and self.m_graph[i][j]>0):  
+                            # If the weight path analized is less than the minimum of the MST
+                            if self.m_graph[i][j] < minimum:
+                                # Defines the new minimum weight, the starting vertex and the ending vertex
+                                minimum = self.m_graph[i][j]
+                                start, end = i, j
+            
+            # Since we added the ending vertex to the MST, it's already selected:
+            selected_nodes[end] = True
+
+            # Filling the MST Adjacency Matrix fields:
+            result[start][end] = minimum
+            
+            if minimum == postitive_inf:
+                result[start][end] = 0
+
+            # print("(%d.) %d - %d: %d" % (indx, start, end, result[start][end]))
+            indx += 1
+            
+            result[end][start] = result[start][end]
+        
+        return result
+    
+    def print_MST(result):
+        # node1, node2, weight in result:
+        for i in range(len(result)):
+            for j in range(0+i, len(result)):
+                if result[i][j] != 0:
+                    print("%d - %d: %d" % (i, j, result[i][j]))
+
+def plot_MST(ax, graph, data):
+    for i in range(len(graph)):
+        for j in range(0+i, len(graph)):
+            if graph[i][j] != 0:
+                ax.plot([data[i][0], data[j][0]],[data[i][1], data[j][1]])
+
+def plot_MSTs(ax, graphs, data):
+    for g in graphs:
+        plot_MST(ax, g, data)
+    # plt.savefig("plots/test_mst.png")
+
+
+with open('data/phate_mnist.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    
+    standard_embedding = []
+    for lines in reader:
+        for row in lines:
+            vals = [float(t) for t in row.strip('][').split(' ') if t.replace(" ", "") != ""]
+            standard_embedding.append(vals)
+
+cluster_labels = hdbscan_low_dim(standard_embedding)
+standard_embedding = standard_embedding[:100]
+cluster_labels = cluster_labels[:100]
+
+def get_cluster_MST(standard_embedding, in_cluster_vector):
+    graph = Graph(num_of_nodes=sum(in_cluster_vector))
+    graph.add_all_nodes(np.array(standard_embedding)[in_cluster_vector])
+    res = graph.prims_mst()
+    return res
+
+print(len(np.unique(cluster_labels)))
+
+clusters_graphs = dict()
+combined_res = Parallel(-1)\
+    (delayed(get_cluster_MST)\
+    (standard_embedding, cluster_labels == label) for label in np.unique(cluster_labels))
+
+print(len(combined_res))
+plot_MSTs(combined_res, standard_embedding)
+
+# nodes = [[x + (random.random()- 0.5)*10, x + (random.random()- 0.5)*10] for x in range(10)]
+# g = Graph(len(nodes))
+# g.add_all_nodes(np.array(nodes))
+# mst = g.prims_mst()
+
+# xs = [x[0] for x in nodes]
+# ys = [x[1] for x in nodes]
+
+# plt.scatter(xs, ys)
+
+# for i in range(len(mst)):
+#     for j in range(0+i, len(mst)):
+#         if mst[i][j] != 0:
+#             plt.plot([nodes[i][0], nodes[j][0]],[nodes[i][1], nodes[j][1]])
+
+# plt.savefig("test.png")
--- mnist_umap_phate.py
+++ mnist_umap_phate.py
@@ -1,31 +1,116 @@
+from joblib import Parallel, delayed
+import matplotlib
 from sklearn.datasets import fetch_openml
 import numpy as np
 import matplotlib.pyplot as plt
+from MST_prim import Graph, plot_MSTs
 import umap
 import hdbscan
 import sklearn.cluster as cluster
 from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 import phate
+from scipy.spatial import ConvexHull, convex_hull_plot_2d
+import numpy as np
+from shapely.ops import unary_union
+import csv
+import pandas as pd
+import matplotlib.colors as mcolors
+from mycolorpy import colorlist as mcp
 
 
-mnist = fetch_openml('mnist_784', version=1)
-mnist.target = mnist.target.astype(int)
+def read_mnist_data():
+    mnist = fetch_openml('mnist_784', version=1)
+    mnist.target = mnist.target.astype(int)
+    return mnist
 
-# standard_embedding = umap.UMAP(random_state=7, min_dist=0.9, n_neighbors=30, n_components=2).fit_transform(mnist.data)
+def calculate_dim_red(data, method: str = 'phate'):
+    assert method in ['phate', 'tsne', 'umap'], f'Dimenionality reduction {method} is not supported.'
+    dr_model = phate.PHATE() if method == 'phate' else umap.UMAP(random_state=7, min_dist=0.9, n_neighbors=30, n_components=2)
+    standard_embedding = dr_model.fit_transform(data)
+    return standard_embedding
 
-phate_op = phate.PHATE()
-standard_embedding = phate_op.fit_transform(mnist.data)
+def hdbscan_low_dim(standard_embedding):
+    # kmeans_labels = cluster.KMeans(n_clusters=10).fit_predict(mnist.data)
+    labels = hdbscan.HDBSCAN(min_samples=10, min_cluster_size=500).fit_predict(standard_embedding)
+    return labels
 
+def draw_clusters(ax, data):
+    num_clusters = data['cluster'].nunique()
+    colors = mcp.gen_color(cmap="Spectral", n=num_clusters)
+    print(num_clusters)
+    for i in range(num_clusters-1):
+        points = data[data['cluster'] == i]
+        if points.shape[0] > 2:
+            hull = ConvexHull(points[['emb1', 'emb2']].to_numpy())
+            vert = np.append(hull.vertices, hull.vertices[0])
 
-plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c=mnist.target.astype(int), s=0.1, cmap='Spectral')
-plt.show()
+            a, b = points['emb1'].iloc[vert], points['emb2'].iloc[vert]
+            ax.plot(a, b, '--', c=colors[i], alpha=0.5)
+            ax.fill(a, b, alpha=0.5, c=colors[i])
 
-kmeans_labels = cluster.KMeans(n_clusters=10).fit_predict(mnist.data)
-plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c=kmeans_labels, s=0.1, cmap='Spectral')
-plt.show()
+def plot(mnist, emb_df, method, standard_embedding, combined_res):
+    fig, ax = plt.subplots(1, figsize=(7, 5))
+    
+    draw_clusters(ax, emb_df)
 
-labels = hdbscan.HDBSCAN(min_samples=10, min_cluster_size=500,).fit_predict(standard_embedding)
-clustered = (labels >= 0)
-# plt.scatter(standard_embedding[~clustered, 0], standard_embedding[~clustered, 1], color=(0.5, 0.5, 0.5),ms=0.1, alpha=0.5)
-plt.scatter(standard_embedding[clustered, 0], standard_embedding[clustered, 1], c=labels[clustered], s=0.1,cmap='Spectral');
-plt.show()
+    colors = mcp.gen_color(cmap="Spectral", n=len(mnist.target.unique()))
+    j = 0
+    for i in mnist.target.unique():
+        plt.scatter(emb_df['emb1'][mnist.target==i], emb_df['emb2'][mnist.target==i], c=colors[j], s=10, label=str(i))
+        j += 1
+        
+    plot_MSTs(ax, combined_res, standard_embedding)
+
+    plt.xlabel(f"{method} 1")
+    plt.ylabel(f"{method} 2")
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
+
+def get_cluster_MST(standard_embedding, in_cluster_vector):
+    graph = Graph(num_of_nodes=sum(in_cluster_vector))
+    graph.add_all_nodes(np.array(standard_embedding)[in_cluster_vector])
+    res = graph.prims_mst()
+    return res
+
+mnist = read_mnist_data()
+
+read = True
+if read:
+    with open('data/phate_mnist.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        
+        standard_embedding = []
+        for lines in reader:
+            for row in lines:
+                vals = [float(t) for t in row.strip('][').split(' ') if t.replace(" ", "") != ""]
+                standard_embedding.append(vals)
+else:
+    standard_embedding = calculate_dim_red(mnist.data)
+
+write = False
+if write:
+    with open('data/phate_mnist.csv', 'w') as f:
+        # create the csv writer
+        writer = csv.writer(f)
+        # write a row to the csv file
+        writer.writerow(standard_embedding)
+
+cluster_labels = hdbscan_low_dim(standard_embedding)
+
+ix = 100
+mnist.data = mnist.data[:ix]
+mnist.target = mnist.target[:ix]
+standard_embedding = standard_embedding[:ix]
+cluster_labels = cluster_labels[:ix]
+
+mnist.data['cluster'] = cluster_labels
+emb_df = pd.DataFrame(standard_embedding, columns =['emb1', 'emb2'])
+emb_df['cluster'] = cluster_labels
+
+clusters_graphs = dict()
+combined_res = Parallel(-1)\
+    (delayed(get_cluster_MST)\
+    (standard_embedding, cluster_labels == label) for label in np.unique(cluster_labels[0]))
+
+plot(mnist, emb_df, 'phate', standard_embedding, combined_res)
