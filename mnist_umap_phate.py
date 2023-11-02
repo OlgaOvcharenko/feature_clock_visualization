ec340fefb1d0b966206faa29282053e21ce76c37
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-11-02 08:32:31 +0100

    Fic

--- mnist_umap_phate.py
+++ mnist_umap_phate.py
@@ -6,14 +6,10 @@ from sklearn.datasets import fetch_openml
 import numpy as np
 import matplotlib.pyplot as plt
 import umap
-from sklearn import preprocessing
 import hdbscan
-import sklearn.cluster as cluster
-from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
 import phate
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
 import numpy as np
-from shapely.ops import unary_union
 import csv
 import pandas as pd
 import matplotlib.colors as mcolors
@@ -21,7 +17,6 @@ from sklearn.linear_model import LinearRegression
 from mycolorpy import colorlist as mcp
 import scanpy as sp
 from skspatial.objects import Line
-from skspatial.plotting import plot_2d
 import math
 
 
@@ -37,7 +32,7 @@ def calculate_dim_red(data, method: str = 'phate'):
     return standard_embedding
 
 def hdbscan_low_dim(standard_embedding):
-    labels = hdbscan.HDBSCAN(min_samples=5, min_cluster_size=40).fit_predict(standard_embedding)
+    labels = hdbscan.HDBSCAN(min_samples=10, min_cluster_size=30).fit_predict(standard_embedding)
     return labels
 
 def draw_clusters(ax, data, alpha):
@@ -53,75 +48,6 @@ def draw_clusters(ax, data, alpha):
                 a, b = points['emb1'].iloc[vert], points['emb2'].iloc[vert]
                 ax.plot(a, b, '--', c=colors[i], alpha=alpha)
                 ax.fill(a, b, alpha=alpha, c=colors[i])
-
-def plot(mnist, emb_df, method, standard_embedding, combined_res):
-    fig, ax = plt.subplots(1, figsize=(7, 5))
-    
-    draw_clusters(ax, emb_df)
-
-    colors = mcp.gen_color(cmap="Spectral", n=len(mnist.target.unique()))
-    j = 0
-    for i in mnist.target.unique():
-        plt.scatter(emb_df['emb1'][mnist.target==i], emb_df['emb2'][mnist.target==i], c=colors[j], s=10, label=str(i))
-        j += 1
-        
-    plot_MSTs(ax, combined_res, standard_embedding)
-
-    plt.xlabel(f"{method} 1")
-    plt.ylabel(f"{method} 2")
-    ax.legend()
-    plt.tight_layout()
-    plt.show()
-
-def get_cluster_MST(standard_embedding, in_cluster_vector):
-    graph = Graph(num_of_nodes=sum(in_cluster_vector))
-    graph.add_all_nodes(np.array(standard_embedding)[in_cluster_vector])
-    res = graph.prims_mst()
-    return res
-
-def main():
-    mnist = read_mnist_data()
-
-    read = True
-    if read:
-        with open('data/phate_mnist.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            
-            standard_embedding = []
-            for lines in reader:
-                for row in lines:
-                    vals = [float(t) for t in row.strip('][').split(' ') if t.replace(" ", "") != ""]
-                    standard_embedding.append(vals)
-    else:
-        standard_embedding = calculate_dim_red(mnist.data)
-
-    write = False
-    if write:
-        with open('data/phate_mnist.csv', 'w') as f:
-            # create the csv writer
-            writer = csv.writer(f)
-            # write a row to the csv file
-            writer.writerow(standard_embedding)
-
-    cluster_labels = hdbscan_low_dim(standard_embedding)
-
-    ix = 100
-    mnist.data = mnist.data[:ix]
-    mnist.target = mnist.target[:ix]
-    standard_embedding = standard_embedding[:ix]
-    cluster_labels = cluster_labels[:ix]
-
-    mnist.data['cluster'] = cluster_labels
-    emb_df = pd.DataFrame(standard_embedding, columns =['emb1', 'emb2'])
-    emb_df['cluster'] = cluster_labels
-
-    clusters_graphs = dict()
-    combined_res = Parallel(-1)\
-        (delayed(get_cluster_MST)\
-        (standard_embedding, cluster_labels == label) for label in np.unique(cluster_labels[0]))
-
-    plot(mnist, emb_df, 'phate', standard_embedding, combined_res)
-
 
 def read_data(path):
     return sp.read(path)  # anndata
@@ -144,38 +70,27 @@ def plot_scd(data, projected_points, min_point, max_point):
     plt.tight_layout()
     plt.show()
 
-def project_line(data, point_a: list=[0, 0], point_b: list=[1, 1]):
-    # min_point = data["emb1"].min() if data["emb1"].min() < data["emb2"].min() else data["emb2"].min()
-    # max_point = data["emb1"].max() if data["emb1"].max() > data["emb2"].max() else data["emb2"].max()
+def project_line(data, angle: float, point_a: list=[0, 0], point_b: list=[1, 1]):
     line = Line.from_points(point_a=point_a, point_b=point_b)
 
     projected_points = []
     for i in range(data.shape[0]):
         point = (data["emb1"][i], data["emb2"][i])
         new_point = line.project_point(point)
-        new_point = rotate(new_point)
+        new_point = rotate(new_point, angle)
         projected_points.append(new_point[0])
     
     return np.array(projected_points)
 
 def get_slope_from_angle(angle: float):
-    res = []
-    if 0 < angle < 90:
-        res = [math.tan(math.radians(angle)) * 100, 100]
-    elif 90 < angle < 180:
-        res = [-math.tan(math.radians(180-angle)) * 100, 100]
-    elif angle == 0 or angle % 180 == 0:
-        res = [100, 0]
-    elif angle % 90 == 0:
-        res = [0, 100]
-    elif angle > 180:
-        raise Exception("Projection angle greater than 180.")
-    return res
+   return [math.cos(math.radians(angle)) * 100, math.sin(math.radians(angle)) * 100]
 
-def rotate(point: list):
-    val = math.sqrt(math.pow(point[0], 2) + math.pow(point[1], 2))
-    x_new = val if point[0] >= 0 else -val
-    y_new = 0
+def rotate(point: list, angle: float):
+    # val = math.sqrt(math.pow(point[0], 2) + math.pow(point[1], 2))
+    # x_new = val if (point[1] >= 0 and point[0] >= 0) else -val
+    # y_new = 0
+    x_new = point[0] * math.cos(math.radians(angle)) + point[1] * math.sin(math.radians(angle))
+    y_new = point[1] * math.cos(math.radians(angle)) - point[0] * math.sin(math.radians(angle))
     return[x_new, y_new]
 
 def get_importance(X, y, significance: float = 0.05):
@@ -218,14 +133,26 @@ def plot_central(data, angles_shift, angles, coefs, is_significant, labels, draw
     ax.add_patch(circle)
     ax.axis('equal')
 
-    coefs_scaled = -radius +  (coefs - coefs.min()) * ((2 * radius) / (coefs.max()-coefs.min()))
+    c_min, c_max = coefs.min(), 0
+
+    if coefs.shape[0] > 1:
+        c_max = coefs.max()
+    
+    elif c_min > 0:
+        c_max = c_min
+        c_min = 0
+    
+    else:
+        c_max = 0
+
+    # coefs_scaled = (((coefs - c_min) / (c_max - c_min)) * (radius - (- radius))) - radius 
+    coefs_scaled = coefs * (radius / max(abs(c_max), abs(c_min)))
     
     for a_s in list(range(0, 360, angles_shift)):
         a_s = math.radians(a_s)
         x_add, y_add = math.cos(a_s) * radius, math.sin(a_s) * radius
         plt.plot((x_center, x_center+x_add), (y_center, y_center+y_add), c='gray', linestyle="--", alpha=0.7, linewidth=1)
 
-    
     arrows = []
     for a, c, s in zip(angles, coefs_scaled, is_significant):
         a = math.radians(a)
@@ -275,7 +202,19 @@ def plot_small(fig, ax, data, angles_shift, angles, coefs, is_significant, label
     ax.add_patch(circle)
     ax.axis('equal')
 
-    coefs_scaled = -radius +  (coefs - coefs.min()) * ((2 * radius) / (coefs.max()-coefs.min()))
+    # coefs_scaled = -radius +  (coefs - coefs.min()) * ((2 * radius) / (coefs.max()-coefs.min()))
+    c_min, c_max = coefs.min(), 0
+
+    if coefs.shape[0] > 1:
+        c_max = coefs.max()
+    
+    elif c_min > 0:
+        c_max = c_min
+        c_min = 0
+    
+    else:
+        c_max = 0
+    coefs_scaled = coefs * (radius / max(abs(c_max), abs(c_min)))
 
     for a_s in list(range(0, 360, angles_shift)):
         a_s = math.radians(a_s)
@@ -338,15 +277,19 @@ def try_scd():
     data["cluster"] = cluster_labels
     data["label"] = res_labels
 
-    angles_shift = 15
-    angles = list(range(0, 180, angles_shift))
-    projections = [project_line(data, point_a=[0, 0], point_b=get_slope_from_angle(angle)) for angle in angles]
-    projections = np.array(projections).T
+    print(data["cluster"].unique())
+    print(data["cluster"].nunique())
 
+    # actual data
+    angles_shift = 1
+    angles = list(range(0, 180, angles_shift))  # FIXME fix angle
+    projections = [project_line(data, angle, point_a=[0, 0], point_b=get_slope_from_angle(angle)) for angle in angles]
+    projections = np.array(projections).T
+    
     plot_central_clock = True
     if plot_central_clock:
-        coefs, pvals, is_significant = get_importance(new_data.to_numpy(), projections)
-        plot_central(data, angles_shift, angles, coefs, is_significant, obs)
+        coefs, _, is_significant = get_importance(new_data.to_numpy(), projections)
+        plot_central(data, 45, angles, coefs, is_significant, obs)
 
     plot_small_clock = True
     if plot_small_clock:
@@ -365,7 +308,7 @@ def try_scd():
             projections_cl = projections[ind[:, 0], :]
 
             coefs, _, is_significant = get_importance(new_data_cl.to_numpy(), projections_cl)
-            arrows = plot_small(fig, ax, data_cl, angles_shift, angles, coefs, is_significant, obs)
+            arrows = plot_small(fig, ax, data_cl, 45, angles, coefs, is_significant, obs)
             arrows_all.extend(arrows)
             
         plt.legend(arrows_all, obs)
