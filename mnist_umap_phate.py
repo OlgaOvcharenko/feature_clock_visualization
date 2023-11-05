1502c27ebd95865b7b518df40bb2aaff971349fb
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-11-05 23:30:04 +0100

    Add results

--- mnist_umap_phate.py
+++ mnist_umap_phate.py
@@ -18,6 +18,8 @@ from mycolorpy import colorlist as mcp
 import scanpy as sp
 from skspatial.objects import Line
 import math
+from sklearn.cluster import DBSCAN
+import matplotlib.patheffects as pe
 
 
 def read_mnist_data():
@@ -35,6 +37,10 @@ def hdbscan_low_dim(standard_embedding):
     labels = hdbscan.HDBSCAN(min_samples=10, min_cluster_size=30).fit_predict(standard_embedding)
     return labels
 
+def dbscan_low_dim(standard_embedding):
+    labels = DBSCAN(min_samples=10).fit_predict(standard_embedding)
+    return labels
+
 def draw_clusters(ax, data, alpha):
     num_clusters = data['cluster'].nunique()
     colors = mcp.gen_color(cmap="Spectral", n=num_clusters)
@@ -46,8 +52,8 @@ def draw_clusters(ax, data, alpha):
                 vert = np.append(hull.vertices, hull.vertices[0])
 
                 a, b = points['emb1'].iloc[vert], points['emb2'].iloc[vert]
-                ax.plot(a, b, '--', c=colors[i], alpha=alpha)
-                ax.fill(a, b, alpha=alpha, c=colors[i])
+                ax.plot(a, b, '--', c="gray", alpha=alpha)
+                ax.fill(a, b, alpha=alpha, c="gray")
 
 def read_data(path):
     return sp.read_h5ad(path)  
@@ -64,6 +70,17 @@ def project_line(data, angle: float, point_a: list=[0, 0], point_b: list=[1, 1])
     
     return np.array(projected_points)
 
+def get_plot(plt, data, draw_hulls: bool = True):
+    fig, ax = plt.subplots(1, figsize=(12, 8))
+    alpha = 0.4
+
+    if draw_hulls:
+        draw_clusters(ax, data, alpha)
+    sc = plt.scatter(data["emb1"], data["emb2"], c=data["label"], cmap="viridis", 
+                     vmin=data["label"].min(), vmax=data["label"].max(), s=3, alpha=alpha)
+    fig.colorbar(sc)
+    return fig, ax
+
 def get_slope_from_angle(angle: float):
    return [math.cos(math.radians(angle)) * 100, math.sin(math.radians(angle)) * 100]
 
@@ -72,19 +89,33 @@ def rotate(point: list, angle: float):
     y_new = point[1] * math.cos(math.radians(angle)) - point[0] * math.sin(math.radians(angle))
     return[x_new, y_new]
 
-def get_importance(X, y, significance: float = 0.05):
+def get_importance(X, y, significance: float = 0.05, univar: bool = False):
     # lm = LinearRegression()
     # lm.fit(X, y)
     # print(lm.coef_)
 
     coefs, pvals, is_significant = [], [], []
-    for i in range(y.shape[1]):
-        # X = sm.add_constant(X)
-        lm = sm.OLS(y[:, i], X).fit()
-        pval = np.array(lm.pvalues)
-        coefs.append(np.array(lm.params))
-        pvals.append(pval)
-        is_significant.append(pval <= significance)
+    if univar:
+        for i in range(y.shape[1]):
+            coefs_a, pvals_a, is_significant_a = [], [], []
+            for j in range(X.shape[1]):
+                lm = sm.OLS(y[:, i], X[:, j]).fit()
+                coefs_a.append(lm.params[0])
+                pvals_a.append(lm.pvalues[0])
+                is_significant_a.append(lm.pvalues[0] <= significance)
+
+            coefs.append(np.array(coefs_a))
+            pvals.append(pvals_a)
+            is_significant.append(is_significant_a)
+
+    else:
+        for i in range(y.shape[1]):
+            # X = sm.add_constant(X)
+            lm = sm.OLS(y[:, i], X).fit()
+            pval = np.array(lm.pvalues)
+            coefs.append(np.array(lm.params))
+            pvals.append(pval)
+            is_significant.append(pval <= significance)
         
     return np.array(coefs), np.array(pvals), np.array(is_significant)
 
@@ -94,26 +125,34 @@ def get_center(data):
 def get_min_max(data):
     return data["emb1"].min(), data["emb1"].max(), data["emb2"].min(), data["emb2"].max()
 
-def plot_central(data, angles_shift, angles, coefs, is_significant, labels, draw_clusters: bool = False, windrose: bool = True):
+def plot_all(data, col_label,label):
     fig, ax = plt.subplots(1, figsize=(12, 8))
     colors = list(mcolors.TABLEAU_COLORS.keys())
-    alpha = 0.4
 
-    if draw_clusters:
-        draw_clusters(ax, data, alpha)
+    sc = plt.scatter(data["emb1"], data["emb2"], c=col_label, cmap="viridis", 
+                     vmin=col_label.min(), vmax=col_label.max(), s=3)
+    fig.colorbar(sc)
+    plt.xlabel(f"UMAP 1")
+    plt.ylabel(f"UMAP 2")
+    plt.title(f"Malignant cells + {label}")
+    plt.tight_layout()
+    plt.show()
+
+def plot_central(data, angles_shift, angles, coefs, is_significant, labels, draw_clusters: bool = False, windrose: bool = True, biggest_arrow: bool = True):
+    fig, ax = plt.subplots(1, figsize=(12, 8))
+    colors = list(mcolors.TABLEAU_COLORS.keys())
+    alpha = 0.2
+
     sc = plt.scatter(data["emb1"], data["emb2"], c=data["label"], cmap="viridis", 
-                     vmin=data["label"].min(), vmax=data["label"].max(), s=3, alpha=alpha)
+                     vmin=data["label"].min(), vmax=data["label"].max(), s=3, alpha=alpha, zorder = 0)
     fig.colorbar(sc)
 
     x_center, y_center = get_center(data)
-    x_min, x_max, y_min, y_max = get_min_max(data)
-    radius = abs(min(x_max-x_min, y_max-y_min)) * 0.5
-    circle = patches.Circle((x_center, y_center), radius=radius, edgecolor='gray', facecolor='aliceblue', linewidth=1, alpha=0.4) 
-    ax.add_patch(circle)
-    ax.axis('equal')
+    # x_min, x_max, y_min, y_max = get_min_max(data)
+    # radius = abs(min(x_max-x_min, y_max-y_min)) * 0.5
+    radius = np.abs(coefs).max()
 
     c_min, c_max = coefs.min(), 0
-
     if coefs.shape[0] > 1:
         c_max = coefs.max()
     
@@ -124,31 +163,50 @@ def plot_central(data, angles_shift, angles, coefs, is_significant, labels, draw
     else:
         c_max = 0
 
-    # coefs_scaled = (((coefs - c_min) / (c_max - c_min)) * (radius - (- radius))) - radius 
+     # Add circles
+    annotate = 0.3
+    num_circles = math.floor(radius / annotate) + 1
+
     coefs_scaled = coefs * (radius / max(abs(c_max), abs(c_min)))
     
     for a_s in list(range(0, 360, angles_shift)):
         a_s = math.radians(a_s)
-        x_add, y_add = math.cos(a_s) * radius, math.sin(a_s) * radius
-        plt.plot((x_center, x_center+x_add), (y_center, y_center+y_add), c='gray', linestyle="--", alpha=0.7, linewidth=1)
+        x_add, y_add = math.cos(a_s) * (num_circles-1) * annotate, math.sin(a_s) * (num_circles-1) * annotate
+        plt.plot((x_center, x_center+x_add), (y_center, y_center+y_add), c='gray', linestyle="--", alpha=0.7, linewidth=1, zorder=10)
     
     if not windrose:
         arrows = []
-        for a, c, s in zip(angles, coefs_scaled, is_significant):
-            a = math.radians(a)
-            x_add, y_add = math.cos(a) * radius, math.sin(a) * radius
 
-            # plot contributions
-            ind = abs(c).argsort(axis=None)[::-1]
-            x_add_coefs, y_add_coefs = math.cos(a) * c[ind], math.sin(a) * c[ind]
+        if not biggest_arrow:
+            for a, c, s in zip(angles, coefs_scaled, is_significant):
+                a = math.radians(a)
+
+                # plot contributions
+                ind = abs(c).argsort(axis=None)[::-1]
+                x_add_coefs, y_add_coefs = math.cos(a) * c[ind], math.sin(a) * c[ind]
+                
+                for is_s, x_c, y_c, i in zip(s[ind], x_add_coefs, y_add_coefs, ind):
+                    if is_s:
+                        col = colors[i]
+                        lbl = labels[i]
+                        arrows.append(plt.arrow(x_center, y_center, x_c, y_c, width=0.01, color=col, label=lbl, zorder=15))
+
+            plt.legend(arrows, labels)
+        
+        else:
+            arrows_ind = np.argmax(np.abs(coefs_scaled), axis=0)
             
-            for is_s, x_c, y_c, i in zip(s[ind], x_add_coefs, y_add_coefs, ind):
-                if is_s:
-                    col = colors[i]
-                    lbl = labels[i]
-                    arrows.append(plt.arrow(x_center, y_center, x_c, y_c, width=0.01, color=col, label=lbl))
+            for i in range(coefs_scaled.shape[1]):
+                a = math.radians(angles[arrows_ind[i]])
 
-        plt.legend(arrows, labels)
+                # plot contributions
+                x_c, y_c = math.cos(a) * coefs_scaled[arrows_ind[i], i], math.sin(a) * coefs_scaled[arrows_ind[i], i]
+                
+                col = colors[i]
+                lbl = labels[i]
+                arrows.append(plt.arrow(x_center, y_center, x_c, y_c, width=0.01, color=col, label=lbl, zorder=15))
+
+            plt.legend(arrows, labels)
 
     else:
         points_x, points_y = [], []
@@ -171,6 +229,22 @@ def plot_central(data, angles_shift, angles, coefs, is_significant, labels, draw
             # ax.fill(points_x[:, i], points_y[:, i], alpha=0.7, c=colors[i])
 
         plt.legend()
+    
+    for i in list(range(1, num_circles))[::-1]:
+        radius_circle = round(i * annotate, 1)
+
+        if i == num_circles-1:
+            circle = patches.Circle((x_center, y_center), radius=radius_circle, edgecolor='gray', linewidth=1, linestyle="--", facecolor=(0.941, 0.973, 1.0, 0.5), fill=True, zorder=10) 
+        else:
+            circle = patches.Circle((x_center, y_center), radius=radius_circle, edgecolor='gray', linewidth=1, linestyle="--", fill=False, zorder=12) 
+
+        ax.add_patch(circle)
+        ax.axis('equal')
+
+        circle_an = math.radians(45)
+        x_ann, y_ann = x_center + math.cos(circle_an) * radius_circle, y_center + math.sin(circle_an) * radius_circle
+        ax.annotate(str(round(radius_circle / (radius / max(abs(c_max), abs(c_min))), 2)), xy=(x_ann, y_ann), ha="right", color="gray",
+                    path_effects=[pe.withStroke(linewidth=2, foreground="white")], zorder=20)
 
     plt.xlabel(f"UMAP 1")
     plt.ylabel(f"UMAP 2")
@@ -185,16 +259,14 @@ def finish_plot():
     plt.tight_layout()
     plt.show()
 
-def plot_small(fig, ax, data, angles_shift, angles, coefs, is_significant, labels):
+def plot_small(fig, ax, data, angles_shift, angles, coefs, is_significant, labels, biggest_arrow: bool = True, scale_circle: float = 1.0, annotate: float = 0.2):
     colors = list(mcolors.TABLEAU_COLORS.keys())
     x_center, y_center = get_center(data)
-    x_min, x_max, y_min, y_max = get_min_max(data)
-    radius = abs(min(x_max-x_min, y_max-y_min)) * 0.5
-    circle = patches.Circle((x_center, y_center), radius=radius, edgecolor='gray', facecolor='aliceblue', linewidth=1, alpha=0.4) 
-    ax.add_patch(circle)
-    ax.axis('equal')
 
-    # coefs_scaled = -radius +  (coefs - coefs.min()) * ((2 * radius) / (coefs.max()-coefs.min()))
+    x_min, x_max, y_min, y_max = get_min_max(data)
+    # scale_circle = abs(max(x_max-x_min, y_max-y_min)) * 0.2
+    radius = np.abs(coefs).max() * scale_circle
+
     c_min, c_max = coefs.min(), 0
 
     if coefs.shape[0] > 1:
@@ -206,28 +278,67 @@ def plot_small(fig, ax, data, angles_shift, angles, coefs, is_significant, label
     
     else:
         c_max = 0
-    coefs_scaled = coefs * (radius / max(abs(c_max), abs(c_min)))
+    
+    # Add circles
+    # annotate = 0.2
+    num_circles = math.floor(radius / annotate) + 1
 
+    coefs_scaled = coefs * (radius / max(abs(c_max), abs(c_min)))
+    
     for a_s in list(range(0, 360, angles_shift)):
         a_s = math.radians(a_s)
-        x_add, y_add = math.cos(a_s) * radius, math.sin(a_s) * radius
-        plt.plot((x_center, x_center+x_add), (y_center, y_center+y_add), c='gray', linestyle="--", alpha=0.7, linewidth=1)
-
+        x_add, y_add = math.cos(a_s) * (num_circles-1) * annotate, math.sin(a_s) * (num_circles-1) * annotate
+        plt.plot((x_center, x_center+x_add), (y_center, y_center+y_add), c='gray', linestyle="--", alpha=0.4, linewidth=1, zorder=10)
     
     arrows = []
-    for a, c, s in zip(angles, coefs_scaled, is_significant):
-        a = math.radians(a)
-        x_add, y_add = math.cos(a) * radius, math.sin(a) * radius
+    if not biggest_arrow:
+        for a, c, s in zip(angles, coefs_scaled, is_significant):
+            a = math.radians(a)
 
-        # plot contributions
-        ind = abs(c).argsort(axis=None)[::-1]
-        x_add_coefs, y_add_coefs = math.cos(a) * c[ind], math.sin(a) * c[ind]
+            # plot contributions
+            ind = abs(c).argsort(axis=None)[::-1]
+            x_add_coefs, y_add_coefs = math.cos(a) * c[ind], math.sin(a) * c[ind]
+            
+            for is_s, x_c, y_c, i in zip(s[ind], x_add_coefs, y_add_coefs, ind):
+                if is_s:
+                    col = colors[i]
+                    lbl = labels[i]
+                    arrows.append(plt.arrow(x_center, y_center, x_c, y_c, width=0.01, color=col, label=lbl, zorder=15))
+
+        plt.legend(arrows, labels)
+    
+    else:
+        arrows_ind = np.argmax(np.abs(coefs_scaled), axis=0)
         
-        for is_s, x_c, y_c, i in zip(s[ind], x_add_coefs, y_add_coefs, ind):
-            if is_s:
-                col = colors[i]
-                lbl = labels[i]
-                arrows.append(plt.arrow(x_center, y_center, x_c, y_c, width=0.05, color=col, label=lbl))
+        for i in range(coefs_scaled.shape[1]):
+            a = math.radians(angles[arrows_ind[i]])
+
+            # plot contributions
+            x_c, y_c = math.cos(a) * coefs_scaled[arrows_ind[i], i], math.sin(a) * coefs_scaled[arrows_ind[i], i]
+            
+            col = colors[i]
+            lbl = labels[i]
+            arrows.append(plt.arrow(x_center, y_center, x_c, y_c, width=0.01, color=col, label=lbl, zorder=15))
+
+        plt.legend(arrows, labels)
+
+    print(list(range(1, num_circles))[::-1])
+    for i in list(range(1, num_circles))[::-1]:
+        radius_circle = round(i * annotate, 1)
+
+        if i == num_circles-1:
+            circle = patches.Circle((x_center, y_center), radius=radius_circle, edgecolor='gray', linewidth=1, linestyle="--", facecolor=(0.941, 0.973, 1.0, 0.5), fill=True, zorder=10) 
+        else:
+            circle = patches.Circle((x_center, y_center), radius=radius_circle, edgecolor='gray', linewidth=1, linestyle="--", fill=False, zorder=12) 
+
+        ax.add_patch(circle)
+        ax.axis('equal')
+
+        circle_an = math.radians(45)
+        x_ann, y_ann = x_center + math.cos(circle_an) * radius_circle, y_center + math.sin(circle_an) * radius_circle
+        ax.annotate(str(round(radius_circle / (radius / max(abs(c_max), abs(c_min))), 2)), xy=(x_ann, y_ann), ha="right", color="gray",
+                    path_effects=[pe.withStroke(linewidth=2, foreground="white")], zorder=20)
+
 
     return arrows
 
@@ -235,8 +346,6 @@ def try_scd():
     # read data
     file_name = 'data/neftel_malignant.h5ad'
     X = read_data(file_name)
-
-    # cut necessary 
 
     # obs = ['genes_expressed', 
     #        'MESlike2', 'MESlike1', 'AClike', 'OPClike', 'NPClike1', 'NPClike2', 
@@ -259,9 +368,10 @@ def try_scd():
     # get labels
     mes = np.stack((X_new.X[:, 0], X_new.X[:,1]))
     npc = np.stack((X_new.X[:, 4], X_new.X[:, 5]))
+    ac, opc = X_new.X[:, 2], X_new.X[:, 3]
     mes_max = np.max(mes, axis=0)
     npc_max = np.max(npc, axis=0)
-    res_vect = np.stack((X_new.X[:, 2], X_new.X[:, 3], mes_max, npc_max))
+    res_vect = np.stack((ac, opc, mes_max, npc_max))
     res_labels = np.max(res_vect, axis=0)
 
     # make data with labels and clusters
@@ -277,15 +387,18 @@ def try_scd():
     data["cluster"] = cluster_labels
     data["label"] = res_labels
 
+    # for col, label in zip([X_new.X[:, 0], X_new.X[:, 1], X_new.X[:, 2], X_new.X[:, 3], X_new.X[:, 4], X_new.X[:, 5]], obs):
+    #     plot_all(data, col, label)
+
     # actual data
     angles_shift = 5
     angles = list(range(0, 180, angles_shift))  # FIXME fix angle
     projections = [project_line(data, angle, point_a=[0, 0], point_b=get_slope_from_angle(angle)) for angle in angles]
     projections = np.array(projections).T
     
-    plot_central_clock = True
+    plot_central_clock = False
     if plot_central_clock:
-        coefs, _, is_significant = get_importance(new_data.to_numpy(), projections)
+        coefs, _, is_significant = get_importance(new_data.to_numpy(), projections, univar=True)
         coefs = (coefs - coefs.mean()) / coefs.std()
         plot_central(data, 45, angles, coefs, is_significant, obs, windrose=False)
 
@@ -298,16 +411,18 @@ def try_scd():
 
         fig, ax = get_plot(plt, data, True)
 
-        for cl in dist_clusters:
+        scale_circle = [1.0, 0.25, 0.3]
+        annotate = [0.2, 0.1, 0.1]
+        for a, scale, cl in zip(annotate, scale_circle, dist_clusters):
             ind = (data["cluster"] == cl).values.reshape((data.shape[0], 1))
 
             data_cl = data[ind]
             new_data_cl = new_data[ind]
             projections_cl = projections[ind[:, 0], :]
 
-            coefs, _, is_significant = get_importance(new_data_cl.to_numpy(), projections_cl)
+            coefs, _, is_significant = get_importance(new_data_cl.to_numpy(), projections_cl, univar=True)
             coefs = (coefs - coefs.mean()) / coefs.std()
-            arrows = plot_small(fig, ax, data_cl, 45, angles, coefs, is_significant, obs)
+            arrows = plot_small(fig, ax, data_cl, 45, angles, coefs, is_significant, obs, scale_circle=scale, annotate=a)
             arrows_all.extend(arrows)
             
         plt.legend(arrows_all, obs)
